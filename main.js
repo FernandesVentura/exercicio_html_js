5f3b7da303941a22e440211f55a1288bc68a6b3d
Fernandes Ventura <fernandes.ventura25@gmail.com>
2024-05-05 19:14:05 +0100

    primeiro commit da quinta atividade
    esta atividade me tirou do sério

---
+++ main.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('form-desafio');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    validaCampo();
+});
+
+function validaCampo(){
+    const campoA = parseInt(document.getElementById('numero-A').value);
+    const campoB = parseInt(document.getElementById('numero-B').value);
+    const mensagemSucesso = `Muito bem, Realmente o valor <b>${campoA}</b> é menor que <b>${campoB}</b>, sendo assimo desafio está completo. Parabéns!` 
+
+    if (isNaN(campoA) || isNaN(campoB)){
+        alert('Os campos precisam estar preenchidos');
+        }
+        if (campoA > campoB) {
+            document.querySelector('.mensagem-erro').style.display = 'block';
+            document.querySelector('.mensagem-sucesso').style.display = 'none';
+
+            document.getElementById('numero-A').value = '';
+            document.getElementById('numero-B').value = '';
+        }else {
+            const containerMensagemSucesso = document.querySelector('.mensagem-sucesso');
+            containerMensagemSucesso.innerHTML = mensagemSucesso;
+            containerMensagemSucesso.style.display = 'block'
+            document.querySelector('.mensagem-erro').style.display = 'none';
+            document.querySelector('.mensagem-sucesso').style.display = 'block';
+
+        }
+    }
+
